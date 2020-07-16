07b75cbfaf1e5d35744bafb7bb58625e584146ee
thoo0224 <thomasplatschorre@gmail.com>
2020-07-16 18:59:44 +0200

    Initial commit

---
+++ src/bin/test.rs
@@ -0,0 +1,23 @@
+use webhook::Webhook;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let webhook = Webhook::from_url("https://discordapp.com/api/webhooks/733322041280954429/HGVZY_KIPyuentLKthM283T9eA6SM4tPM3S8r_r7ajHEG8Tb5nPAIPAKOSGmqwSWOo9P");
+    webhook.send(|m| m.
+        content("Test")
+        .username("ThooOk")
+        .avatar_url("https://cdn.discordapp.com/avatars/557337479523598340/e80851184b12242bd373ab5f6163c57b.png?size=128")
+        .embed(|e| e.
+            title("Test")
+            .color(0x32a852)
+            .field("Field1", "Value1", false)
+            .field("Field2", "Value2", false)
+            .video("https://cdn.discordapp.com/attachments/718702021766676512/733363175260422174/faggot.mp4", None, None)
+            //.author("Author", "",Some("https://cdn.discordapp.com/avatars/557337479523598340/e80851184b12242bd373ab5f6163c57b.png?size=128".to_owned()), None)
+            //.image("https://cdn.discordapp.com/avatars/557337479523598340/e80851184b12242bd373ab5f6163c57b.png?size=128", None, None, None)
+            .footer("Footer", Some("https://cdn.discordapp.com/avatars/557337479523598340/e80851184b12242bd373ab5f6163c57b.png?size=128".to_owned()), None)
+        )
+    ).await?;
+    Ok(())
+}
---
+++ src/lib.rs
@@ -0,0 +1,381 @@
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+
+pub struct Webhook {
+    url: String,
+    client: Client
+}
+
+#[derive(Deserialize)]
+#[derive(Debug)]
+pub struct WebhookModel {
+    pub name: String,
+    #[serde(rename = "type")]
+    pub webhook_type: i8,
+    pub channel_id: String,
+    pub token: Option<String>,
+    pub avatar: Option<String>,
+    pub guild_id: Option<String>,
+    pub user: Option<WebhookUser>
+}
+
+#[derive(Deserialize, Debug)]
+pub struct WebhookUser {
+    pub username: String,
+    pub discriminator: String,
+    pub id: String,
+    pub avatar: String
+}
+
+trait Sendable {
+    fn string_content(&self) -> String ;
+}
+
+type OString = Option<String>;
+type OInt32 = Option<i32>;
+type OBool = Option<bool>;
+
+#[derive(Debug, Serialize)]
+pub struct EmbedBuilder {
+    title: OString,
+    #[serde(rename = "type")]
+    type_: OString,
+    description: OString,
+    url: OString,
+    timestamp: OString,
+    color: OInt32,
+    fields: Vec<EmbedField>,
+    footer: Option<EmbedFooter>,
+    image: Option<EmbedImage>,
+    author: Option<EmbedAuthor>,
+    video: Option<EmbedVideo>
+}
+
+#[derive(Debug, Serialize)]
+pub struct Embed {
+    title: OString,
+    #[serde(rename = "type")]
+    type_: OString,
+    description: OString,
+    url: OString,
+    timestamp: OString,
+    color: OInt32,
+    fields: Vec<EmbedField>,
+    footer: Option<EmbedFooter>,
+    image: Option<EmbedImage>,
+    author: Option<EmbedAuthor>,
+    video: Option<EmbedVideo>
+}
+
+impl EmbedBuilder {
+
+    pub fn new() -> Self {
+        Self {
+            title: None,
+            type_: None,
+            description: None,
+            url: None,
+            timestamp: None,
+            color: None,
+            fields: vec![],
+            footer: None,
+            image: None,
+            author: None,
+            video: None
+        }
+    }
+
+    pub fn title(&mut self, title: &str) -> &mut EmbedBuilder {
+        self.title = Some(title.to_owned());
+        self
+    }
+
+    pub fn type_(&mut self, type_: &str) -> &mut EmbedBuilder {
+        self.type_ = Some(type_.to_owned());
+        self
+    }
+
+    pub fn description(&mut self, description: &str) -> &mut EmbedBuilder {
+        self.description = Some(description.to_owned());
+        self
+    }
+
+    pub fn url(&mut self, url: &str) -> &mut EmbedBuilder {
+        self.url = Some(url.to_owned());
+        self
+    }
+
+    pub fn color(&mut self, color: i32) -> &mut EmbedBuilder {
+        self.color = Some(color);
+        self
+    }
+
+    pub fn field(&mut self, name: &str, value: &str, inline: bool) -> &mut EmbedBuilder {
+        self.fields.push(EmbedField::new(name, value, inline));
+        self
+    }
+
+    pub fn timestamp(&mut self, timestamp: &str) -> &mut EmbedBuilder {
+        self.timestamp = Some(timestamp.to_owned());
+        self
+    }
+
+    pub fn footer(&mut self, text: &str, url: OString, proxy_url: OString) -> &mut EmbedBuilder {
+        self.footer = Some(EmbedFooter::new(text, url, proxy_url));
+        self
+    }
+
+    pub fn image(&mut self, url: &str, proxy_url: OString, height: OInt32, width: OInt32) -> &mut EmbedBuilder {
+        self.image = Some(EmbedImage::new(url, proxy_url, height, width));
+        self
+    }
+
+    pub fn author(&mut self, name: &str, url: &str, icon_url: OString, proxy_icon_url: OString) -> &mut EmbedBuilder {
+        self.author = Some(EmbedAuthor::new(name, url, icon_url, proxy_icon_url));
+        self
+    }
+
+    pub fn video(&mut self, url: &str, height: OInt32, width: OInt32) -> &mut EmbedBuilder {
+        self.video = Some(EmbedVideo::new(url, height, width));
+        self
+    }
+
+    pub fn build(&mut self) -> Embed {
+        Embed {
+            title: self.title.clone(),
+            type_: self.type_.clone(),
+            description: self.description.clone(),
+            url: self.url.clone(),
+            timestamp: self.timestamp.clone(),
+            color: self.color.clone(),
+            fields: self.fields.clone(),
+            footer: self.footer.clone(),
+            image: self.image.clone(),
+            author: self.author.clone(),
+            video: self.video.clone()
+        }
+    }
+
+}
+
+#[derive(Debug, Serialize)]
+pub struct EmbedField {
+    name: String,
+    value: String,
+    inline: bool
+}
+
+impl EmbedField {
+    pub fn new(name: &str, value: &str, inline: bool) -> Self {
+        Self {
+            name: name.to_owned(),
+            value: value.to_owned(),
+            inline
+        }
+    }
+}
+
+impl Clone for EmbedField {
+    fn clone(&self) -> Self {
+        Self {
+            name: self.name.clone(),
+            value: self.value.clone(),
+            inline: self.inline.clone()
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct EmbedFooter {
+    text: String,
+    icon_url: OString,
+    proxy_icon_url: OString
+}
+
+impl EmbedFooter {
+    pub fn new(text: &str, icon_url: Option<String>, proxy_icon_url: Option<String>) -> Self {
+        Self {
+            text: text.to_owned(),
+            icon_url,
+            proxy_icon_url
+        }
+    }
+}
+
+impl Clone for EmbedFooter {
+    fn clone(&self) -> Self {
+        Self {
+            text: self.text.clone(),
+            icon_url: self.icon_url.clone(),
+            proxy_icon_url: self.proxy_icon_url.clone()
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct EmbedImage {
+    url: OString,
+    proxy_url: OString,
+    height: OInt32,
+    width: OInt32
+}
+
+impl EmbedImage {
+    pub fn new(url: &str, proxy_url: OString, height: OInt32, width: OInt32) -> Self {
+        Self {
+            url: Some(url.to_owned()),
+            proxy_url,
+            height,
+            width
+        }
+    }
+}
+
+impl Clone for EmbedImage {
+    fn clone(&self) -> Self {
+        Self {
+            url: self.url.clone(),
+            proxy_url: self.proxy_url.clone(),
+            height: self.height.clone(),
+            width: self.width.clone()
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct EmbedAuthor {
+    name: String,
+    url: String,
+    icon_url: OString,
+    proxy_icon_url: OString
+}
+
+impl EmbedAuthor {
+    pub fn new(name: &str, url: &str, icon_url: OString, proxy_icon_url: OString) -> Self {
+        Self {
+            name: name.to_owned(),
+            url: url.to_owned(),
+            icon_url,
+            proxy_icon_url
+        }
+    }
+}
+
+impl Clone for EmbedAuthor {
+    fn clone(&self) -> Self {
+        Self {
+            name: self.name.clone(),
+            url: self.url.clone(),
+            icon_url: self.icon_url.clone(),
+            proxy_icon_url: self.proxy_icon_url.clone()
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct EmbedVideo {
+    url: String,
+    height: OInt32,
+    width: OInt32
+}
+
+impl EmbedVideo {
+    pub fn new(url: &str, height: OInt32, width: OInt32) -> Self {
+        Self {
+            url: url.to_owned(),
+            height,
+            width
+        }
+    }
+}
+
+impl Clone for EmbedVideo {
+    fn clone(&self) -> Self {
+        Self {
+            url: self.url.clone(),
+            height: self.height.clone(),
+            width: self.width.clone()
+        }
+    }
+}
+
+#[derive(Debug, Serialize)]
+pub struct Message {
+    content: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    username: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    avatar_url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    tts: Option<bool>,
+    embeds: Vec<Embed>
+}
+
+impl Message {
+
+    pub fn new() -> Self {
+        Self {
+            content: None,
+            username: None,
+            avatar_url: None,
+            tts: None,
+            embeds: vec![]
+        }
+    }
+
+    pub fn content(&mut self, content: &str) -> &mut Message {
+        self.content = Some(content.to_owned());
+        self
+    }
+
+    pub fn username(&mut self, name: &str) -> &mut Message {
+        self.username = Some(name.to_owned());
+        self
+    }
+
+    pub fn avatar_url(&mut self, url: &str) -> &mut Message {
+        self.avatar_url = Some(url.to_owned());
+        self
+    }
+
+    pub fn tts(&mut self, tts: bool) -> &mut Message {
+        self.tts = Some(tts.to_owned());
+        self
+    }
+
+    pub fn embed<F>(&mut self, embed: F) -> &mut Message
+    where F: Fn(&mut EmbedBuilder) -> &mut EmbedBuilder {
+        let mut em = EmbedBuilder::new();
+        let mut embed = embed(&mut em);
+        self.embeds.push(embed.build());
+        self
+    }
+
+}
+
+impl Webhook {
+
+    pub fn from_url(url: &str) -> Self {
+        Self {
+            url: url.to_owned(),
+            client: Client::new()
+        }
+    }
+
+    pub async fn get_info(&self) -> Result<WebhookModel, Box<dyn Error>> {
+        let request = self.client.get(&self.url).send().await?;
+        let content = request.text().await?;
+        Ok(serde_json::from_str(&content)?)
+    }
+
+    pub async fn send<F>(&self, t: F) -> Result<(), Box<dyn Error>>
+    where F: Fn(&mut Message) -> &mut Message {
+        let mut msg = Message::new();
+        let message = t(&mut msg);
+        let request = self.client.post(&self.url)
+            .json(&message).send().await?;
+        Ok(())
+    }
+
+}
